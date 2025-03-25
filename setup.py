4447ca51afb5ade87d3d7dbe1ce31781f6301aa1
yesh747 <ychillakuru@gmail.com>
2025-03-25 11:37:51 -0400

    dataframe update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pubmedquery',
-    version='0.1.5',
+    version='0.1.6',
     author='Yeshwant Chillakuru',
     description='A Python package to download data from PubMed',
     packages=find_packages(),  # Automatically discovers all packages with `__init__.py`
